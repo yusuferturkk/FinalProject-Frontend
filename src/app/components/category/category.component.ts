0383a61e05c4c8791f1ac30ad0944c0264c58246
Yusuf Ertürk <yusuf_erturk98@hotmail.com>
2022-03-20 04:11:25 +0300

    18. Gün - Ders

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  categories: Category[] = [];
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => (
+      this.categories = response.data));
+  }
 }
